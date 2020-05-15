fe93c473a430177cdbb103bae701fd6709e5ff14
Brandon Kelly <brandon@pixelandtonic.com>
2020-05-15 08:46:23 -0700

    Introduction link

--- .vuepress/config-en.js
+++ .vuepress/config-en.js
@@ -17,6 +17,7 @@ module.exports = {
                 title: 'Introduction',
                 collapsable: false,
                 children: [
+                    ['', 'Introduction'],
                     'editions',
                 ]
             },
